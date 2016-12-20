f1692b4e5b900640d1f26760a4322fa7a330c024
Atul Golhar <atulgolhar@gmail.com>
2016-12-20 14:11:24 -0500

    Create capture.py

---
+++ capture.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#capture.py
+
+import io
+import sys
+
+sys.stdout = io.StringIO()
+
+print("An error message", file=sys.stdout)
+sys.stdout.write("Another error message\n")
+print("Nothing has been printed on the console", file=sys.stdout)
+
+error_strings = sys.stdout.getvalue()
+sys.stdout = sys.__stdout__
+print(error_strings)
