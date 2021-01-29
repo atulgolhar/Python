ad9510bcb8bddb553482a72e315fa2dc9f4e03c8
Atul Golhar <atulgolhar@gmail.com>
2021-01-29 16:43:34 -0500

    Create IndexingDate_mvp.py

---
+++ IndexingDate_mvp.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# IndexingDate_mvp.py
+
+# Prints out a date, given user input of year, month, and day as numbers.
+
+months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'Novembe',
+    'December']
+
+year = input('Year 4 digits: ')
+month = input('Month (1-12): ')
+day = input('Day (1-31): ')
+
+print(month + '-' + day + '-' + year)
