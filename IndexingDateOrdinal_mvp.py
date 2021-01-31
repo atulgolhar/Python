f3ffbb28ff1231cfc92ee5017cc5f45d89de5ac1
Atul Golhar <atulgolhar@gmail.com>
2021-01-30 22:36:07 -0500

    Update IndexingDateOrdinal_mvp.py

--- IndexingDateOrdinal_mvp.py
+++ IndexingDateOrdinal_mvp.py
@@ -25,6 +25,7 @@ day_number = int(day)
 month_number = int(month)
 year_number = int(year)
 
+# self check
 # details of ordinal day written in english
 # 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
 # 1st
@@ -38,7 +39,8 @@ year_number = int(year)
 # 31st
 endings = ['st', 'nd', 'rd'] + 17*['th'] + ['st', 'nd', 'rd'] + 7*['th'] + ['st']
 
-# len(endings)
+# self check
+# len(endings) 
 # 31
 
 print("String version", month + '-' + day + '-' + year)
@@ -51,7 +53,7 @@ print("variable months: ", month_name, "type: ", type(month_name))
 print("variable month_number: ", month_number, "type: ", type(month_number))
 print("variable year_number: ", year_number, "type", type(year_number))
 
-ordinal = day + endings[day_number-1]
+ordinal = day + endings[day_number-1]   # offset day_number to grad correct index
 
 print("Corrected Version: ")
 
