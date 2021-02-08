1eece617a103f914de6035b527055b4563731e37
Atul Golhar <atulgolhar@gmail.com>
2021-02-08 13:36:05 -0500

    Create sentenceInsideCenteredBox.py

---
+++ sentenceInsideCenteredBox.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# sentenceInsideCenteredBox.py
+# Example of printing a sentence in a centered box 
+# of correct width
+
+sentence = input('Sentence: ')
+
+screen_width    = 80
+text_width      = len(sentence)
+box_width       = text_width + 6
+left_margin     = (screen_width - box_width) // 2
+
+print()
+print(' ' * left_margin + '+'  + '-' * (box_width-2) +    '+')
+print(' ' * left_margin + '| ' + ' ' * text_width    + '   |')
+print(' ' * left_margin + '| ' +       sentence      + '   |')
+print(' ' * left_margin + '| ' + ' ' * text_width    + '   |')
+print(' ' * left_margin + '+'  + '-' * (box_width-2) +    '+')
+print()
