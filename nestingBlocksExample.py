564ed027f694871dd0487cf777a58a5973ef59c6
Atul Golhar <atulgolhar@gmail.com>
2021-02-19 18:05:15 -0500

    Update nestingBlocksExample.py

--- nestingBlocksExample.py
+++ nestingBlocksExample.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# nestingBlocksExample.py
+# nestingBlocks.py
 
 # simple example, Chapter 5, pg 79
 # ask user for one name only and
@@ -9,10 +9,13 @@
 name = input('What is your name? ')
 if name.endswith('Gumby'):
     if name.startswith('Mr.'):
-        print('Hello, Mr. Gumby')
+        print('Hello, Mr. Gumby')       # two conditions to get here
     elif name.startswith('Mrs.'):
-        print('Hello, Mrs. Gumby')
+        print('Hello, Mrs. Gumby')      # two conditions to get here
     else:
-        print('Hello kid Gumby')
+        print('Hello kid Gumby')        # only one condition to get here
 else:
-        print('hello stranager')
+        print('hello stranager')        # no conditions
+
+# note that error checking would make inputs more robust
+# can enter R2D2, _______, #*$*#%, or any characters
