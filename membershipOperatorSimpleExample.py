f8fc00c4ca3cdf0fbc575bc6d4d2586ea3dad8f4
Atul Golhar <atulgolhar@gmail.com>
2021-02-19 18:34:32 -0500

    Create membershipOperatorSimpleExample.py

---
+++ membershipOperatorSimpleExample.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# membershipOperatorSimpleExample.py
+
+# simple example, Chapter 5, pg 82
+
+name = input('What is your name? ')
+if 's' is name:
+    print('Your name contains the letter "s".')
+else:
+    print('Your name does not contain the letter "s".')
