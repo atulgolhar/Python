f7e2481120dbc6eebb7d31f799fe610341cf1ac6
Atul Golhar <atulgolhar@gmail.com>
2021-02-19 17:44:47 -0500

    Create elifExample.py

---
+++ elifExample.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# elifExample.py Simple example
+
+num = int(input('Enter a number: '))
+if num > 0:
+    print('This number is positive +')
+elif num < 0:
+    print('This number is negative -')
+else:
+    print('The number is zero.')
