25db3d480b943fe1a76ef3981a2db2885e24ce2d
Atul Golhar <atulgolhar@gmail.com>
2021-02-24 15:58:59 -0500

    Create fibonacciDynamic.py

---
+++ fibonacciDynamic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# fibonacciDynamic.py
+
+fibs = [0,1]
+
+num = int(input('How many Fibonacci numbers do you want? '))
+
+for i in range(num-2):
+    fibs.append(fibs[-2] + fibs[-1])
+
+print(fibs)
+print(len(fibs))
