dd42222ea95ccbab9b764d52b347eac3a810d05c
Atul Golhar <atulgolhar@gmail.com>
2021-02-25 12:39:59 -0500

    Create fibonacciAbstraction.py

---
+++ fibonacciAbstraction.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# fibonacciAbstraction.py
+
+def fibs(num):
+    result = [0,1]
+    for i in range(num-2):
+        result.append(result[-2] + result[-1])
+    return result
+    
+    
+>>> print(fibs(10))
