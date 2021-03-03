9c94236de0acb7b635de7179643fb42a86b448a4
Atul Golhar <atulgolhar@gmail.com>
2021-03-02 19:10:23 -0500

    Create simpleLambda.py

---
+++ simpleLambda.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# simpleLambda.py
+
+# simple Lambda usage
+
+>>> (lambda x: x + 1)(2)
+3
+
+>>> add_one = lambda x: x+1
+>>> add_one(3)
+4
+
+>>> def add_oneFunction(x):
+    return x + 1
+
+>>> add_oneFunction(5)
+6
