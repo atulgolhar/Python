5ab224a5c8040f9b54b611f2d518861a1988b2e4
Atul Golhar <atulgolhar@gmail.com>
2021-03-03 09:13:50 -0500

    Create os.system().py

---
+++ os.system().py
@@ -0,0 +1,10 @@
+# os.system WIP
+
+>>> import os
+>>> os.system(print('Hello World'))
+Hello World
+Traceback (most recent call last):
+  File "<pyshell#101>", line 1, in <module>
+    os.system(print('Hello World'))
+TypeError: expected str, bytes or os.PathLike object, not NoneType
+>>> 
