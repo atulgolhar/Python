732b46b66c5bbc0009d073c1c1cb67cc5ceec9dd
Atul Golhar <atulgolhar@gmail.com>
2021-04-06 16:47:58 -0400

    Update reverseargs.py

--- reverseargs.py
+++ reverseargs.py
@@ -2,9 +2,10 @@
 
 # reverseargs.py
 import sys
-args = sys.argv[1:]
-args.reverse()
-print(' '.join(args))
+args = sys.argv[1:]     # make copy and skip first element
+args.reverse()          # reverse the list
+print(' '.join(args))   # not able to print an in-place modication thus created a copy
 
+# Example
 $ python reverseargs.py this is a test
 test a is this
